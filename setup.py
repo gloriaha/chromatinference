3716968e61e447f5b0245e44b5a683b513b23809
gloriaha <gloha96@gmail.com>
2020-04-13 00:40:33 -0400

    add minimal structure and dependencies
    based on example package.  need to change test_io.py and data_io.py after adding data.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='error_correction',
+      version='1.0',
+      description='Analyze chromosome segregation error data',
+      url='http://github.com/phys201/error_correction',
+      author='gloriaha',
+      author_email='gloriaha@users.noreply.github.com',
+      license='GPLv3',
+      packages=['error_correction'],
+      install_requires=['numpy', 'matplotlib', 'pandas', 'scipy', 'seaborn', 'pymc3', 'emcee', 'os', 'unittest']
+      zip_safe=False)
