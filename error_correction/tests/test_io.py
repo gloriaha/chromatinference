6f141ffff3856a21dd1fbc0e4d62efcfc7b7e32d
1ynd0n <63434995+1ynd0n@users.noreply.github.com>
2020-04-15 09:53:05 -0400

    model_tests_tutorial
    I added model.py, added the test functions, and modified the tutorial

--- error_correction/tests/test_io.py
+++ error_correction/tests/test_io.py
@@ -1,8 +1,23 @@
 from unittest import TestCase
-from error_correction.data_io import get_example_data_file_path, load_data
+from error_correction.data_io import SyntheticData
+from error_correction.model import logLikeIndDelta
 import pandas as  pd
+import numpy as np
+
+
+dir=''
 
 class TestIo(TestCase):
     def test_data_io(self):
-        data = load_data(get_example_data_file_path('example_data.txt'))
-        assert data.x[0] == 1
+        data = SyntheticData(dir+'params_test.yml',
+                             dir+'data_test.txt')
+        assert data.data['errors'][0] == 1
+
+class TestIo2(TestCase):
+    def test_data_io(self):
+        data = SyntheticData(dir+'params_test.yml',
+                             dir+'data_test.txt')
+        pee = np.random.rand()
+        logp = np.log(pee)
+        logLike = logLikeIndDelta(data,pee)
+        assert logp == logLike
