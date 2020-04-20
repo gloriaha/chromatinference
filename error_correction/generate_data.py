d83709bba7ae664f8832904f2f9698f1de400b29
gloriaha <gloha96@gmail.com>
2020-04-20 15:13:48 -0400

    changed hierarchy of data generation

--- error_correction/generate_data.py
+++ error_correction/generate_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.stats as st
 import pandas as pd
+from error_correction.data_io import get_example_data_file_path
+import yaml
 
 def dNk_from_errors(errors, p_left=0.5):
     """
@@ -13,9 +15,12 @@ def dNk_from_errors(errors, p_left=0.5):
     """
     # choose random numbers to determine if missegregations go left or right
     random_choice = [np.random.random(int(n)) for n in errors]
+    # store cell 1, cell 2 kinetochore numbers
+    N_1 = np.array([2*np.sum(r>p_left) for r in random_choice])
+    N_2 = np.array([2*np.sum(r<=p_left) for r in random_choice])
     # convert into kinetochore count diferences
-    dN_k = [2*np.abs(np.sum(r>p_left)-np.sum(r<=p_left)) for r in random_choice]
-    return np.array(dN_k)
+    dN_k = np.abs(N_1-N_2)
+    return N_1, N_2, dN_k
 
 def generate_independent_data(params, p_left=0.5):
     """
@@ -36,11 +41,13 @@ def generate_independent_data(params, p_left=0.5):
     # randomly choose number of errors for each cell
     errors = st.binom.rvs(n_chrom, size=n_cells, p=p_misseg)
     # convert to kinetochore count difference
-    dNk = dNk_from_errors(errors, p_left)
+    N_1, N_2, dNk = dNk_from_errors(errors, p_left)
     # add Poisson noise
     dNk_w_noise = dNk + st.poisson.rvs(size=n_cells, mu=1)
     # write to dataframe
     df = pd.DataFrame({'errors' : errors,
+                       'N_1' : N_1,
+                       'N_2' : N_2,
                       'dNk' : dNk,
                       'dNk_w_noise' : dNk_w_noise})
     return df
@@ -70,11 +77,44 @@ def generate_catastrophe_data(params, p_left=0.5):
     errors[:n_cat]= st.binom.rvs(n_chrom, size=n_cat, p=p_misseg)+C
     errors[n_cat:] = st.binom.rvs(n_chrom, size=n_cells-n_cat, p=p_misseg)
     # convert to kinetochore count differences
-    dNk = dNk_from_errors(errors, p_left)
+    N_1, N_2, dNk = dNk_from_errors(errors, p_left)
     # add Poisson noise
     dNk_w_noise = dNk + st.poisson.rvs(size=n_cells, mu=1)
     # write to dataframe
     df = pd.DataFrame({'errors' : errors.astype(int),
+                       'N_1' : N_1,
+                       'N_2' : N_2,
                       'dNk' : dNk,
                       'dNk_w_noise' : dNk_w_noise})
     return df
+
+class GenerateData:
+    def __init__(self, model, params, name, data_dir):
+        self.data, self.params = self.make_data(model, params, name, data_dir)
+    def make_data(self, model, params, name, data_dir):
+        # store parameter names
+        param_names = ['p_misseg', 'n_cells', 'n_chrom', 'p_cat', 'C']
+        # set up file path
+        data_path = get_example_data_file_path('data_'+str(name)+'.txt', data_dir)
+        param_path = get_example_data_file_path('params_'+str(name)+'.yml', data_dir)
+        # generate and write data files
+        if model == 'independent':
+            param_dict = {name:param for name, param in zip(param_names[:3], params)}
+            df = generate_independent_data(params)
+            with open(data_path, 'w') as f:
+                f.write('# independent model\n')
+                f.write(df.to_string(index=None))
+            with open(param_path, 'w') as f:
+                yaml.dump(param_dict, f, default_flow_style=False)
+            return df, param_dict
+        elif model == 'catastrophe':
+            param_dict = {name:param for name, param in zip(param_names, params)}
+            df = generate_catastrophe_data(params)
+            with open(data_path, 'w') as f:
+                f.write('# catastrophe model\n')
+                f.write(df.to_string(index=None))
+            with open(param_path, 'w') as f:
+                yaml.dump(param_dict, f, default_flow_style=False)
+            return df, param_dict
+        else:
+            print('Model name should be independent or catastrophe')
