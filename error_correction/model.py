92208f3f7ddbdd863ed7d459118f990238ee783b
gloriaha <gloha96@gmail.com>
2020-04-23 15:13:57 -0400

    added more walkers to emcee, changed initial condition, put burn-in function in model.py

--- error_correction/model.py
+++ error_correction/model.py
@@ -219,12 +219,15 @@ def emcee_biased_fit(data, nwalkers=50, nsteps=2000):
     ndim = 2
     # set up starting positions
     gaussian_ball = 1.e-3 * np.random.randn(nwalkers, ndim)
-    starting_positions = (1 + gaussian_ball) * [0.01,0.25]
+    starting_positions = (1 + gaussian_ball) * [0.05,0.25]
     # run sampler
     sampler = emcee.EnsembleSampler
     first_argument = nwalkers
     sampler = sampler(first_argument, ndim, logPostBiasedDelta, args=(dNk, n_chrom, n_cells))
-    sampler.run_mcmc(starting_positions, nsteps)
+    for i, result in enumerate(sampler.sample(starting_positions, iterations=nsteps)):
+        if (i+1) % 100 == 0:
+            print("{0:5.1%}".format(float(i+1) / nsteps))
+    #sampler.run_mcmc(starting_positions, nsteps)
     
     #df = pd.DataFrame(np.vstack(sampler.chain))
     #df.index = pd.MultiIndex.from_product([range(nwalkers), range(nsteps)], names=['walker', 'step'])
@@ -258,7 +261,20 @@ def burnInPlotAffine(nTheta, sampler):
         for j in range(nTheta):
             sns.lineplot(xVals, sampler.chain[i,:,j], ax=axs[j])
 
-
+def delete_burn_in(sampler, n_pts, n_dim):
+    """
+    deletes burn in samples from emcee sampler
+    inputs
+        sampler: emcee sampler object
+        n_pts : number of points to cut off from beginning, int
+        n_dim : number of parameters, int
+    output
+        df : dataframe containing burned in results
+    """
+    samples = sampler.chain[:,n_pts:,:]
+    traces = samples.reshape(-1, n_dim).T
+    df = pd.DataFrame({'p_misseg': traces[0], 'p_left': traces[1]})
+    return df
     
 def logLikeGivenCatDelta(data, params):
     '''
