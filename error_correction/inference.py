2ded263e2c30f1f736a5e05887f2d4ecac8eda1b
gloriaha <gloha96@gmail.com>
2020-04-23 16:47:32 -0400

    update tutorial.ipynb after file reorganization

--- error_correction/inference.py
+++ error_correction/inference.py
@@ -28,13 +28,15 @@ def emcee_biased_fit(data, nwalkers=50, nsteps=2000):
     sampler = emcee.EnsembleSampler
     first_argument = nwalkers
     sampler = sampler(first_argument, ndim, logPostBiasedDelta, args=(dNk, n_chrom, n_cells))
-    sampler.run_mcmc(starting_positions, nsteps)
+    for i, result in enumerate(sampler.sample(starting_positions, iterations=nsteps)):
+        if (i+1) % 100 == 0:
+            print("{0:5.1%}".format(float(i+1) / nsteps))
+    #sampler.run_mcmc(starting_positions, nsteps)
     
     #df = pd.DataFrame(np.vstack(sampler.chain))
     #df.index = pd.MultiIndex.from_product([range(nwalkers), range(nsteps)], names=['walker', 'step'])
     #df.columns = ['p_misseg', 'p_left']
     return sampler
-
 
 
 def burnInPlotAffine(nTheta, sampler):
